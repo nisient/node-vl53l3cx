ba020d7156dc388fee14fbf59ce44855a79dbe25
Rob Gillan <rob@nisient.com>
2021-06-17 14:00:59 +1000

    no message

--- src/vl53l3cx.cpp
+++ src/vl53l3cx.cpp
@@ -133,7 +133,7 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
 	);
 	exports.Set(
 		Napi::String::New(env, "readSensor"),
-		Napi::Function:New(env, readSensor)
+		Napi::Function::New(env, readSensor)
 	);
 	
 	return exports;
