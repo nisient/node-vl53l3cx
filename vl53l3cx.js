c0b34d1821b0d7fae42a6a15276d35a28a450783
Rob Gillan <rob@nisient.com>
2021-06-17 13:40:29 +1000

    test 1

--- vl53l3cx.js
+++ vl53l3cx.js
@@ -3,3 +3,9 @@ const addonVL53L3CX = require('bindings')('node-vl53l3cx');
 
 // expose module API
 exports.initSensor = addonVL53L3CX.initSensor;
+
+console.log('exports:', addonVL53L3CX);
+console.log();
+
+console.log('initSensor:',  addonVL53L3CX.initSensor());
+console.log();
